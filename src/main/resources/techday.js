7346da06066f97d7279ec43fa0cc1b791b7a445d
Martin Sander <martin.sander@tngtech.com>
2010-11-11 19:39:55 +0000

    Move from text to images. Da habt ihr euern Willen.

--- src/main/resources/techday.js
+++ src/main/resources/techday.js
@@ -9,7 +9,7 @@
             var talkId = that.attr("id").replace(/[^0-9]/g,"");
             var interested = (that.attr("data-interest") === "true");
             var audience = that.parent().parent().find("td[ id ^= 'audience']");
-            var interestText = getInterestText(interested);
+            var interestImage = getInterestImage(interested);
             var contextPath = AJS.params.contextPath;
 
             // TODO use property for url
@@ -24,7 +24,8 @@
             $.ajax({type: "POST", dataType: "json", url: url, data: "",
             success: function(data) {
                 that.attr("data-interest", !interested);
-                that.text(interestText);
+                var img = that.find("img")
+                img.attr("src", interestImage)
                 audience.text(data.userNo);
                 audience.attr("title", data.users);
                 if (interested) {
@@ -37,14 +38,13 @@
             return false;
         });
 
-
-        var getInterestText = function(interested) {
-            if (interested) {
-                return AJS.params.notInterestedLabel;
+        var getInterestImage = function(interested) {
+        	if (interested) {
+        		return AJS.params.interestedImage;
+            } else {
+            	return AJS.params.notInterestedImage;
             }
-            return AJS.params.interestedLabel;
         }
-
     })
 
 })(jQuery);
