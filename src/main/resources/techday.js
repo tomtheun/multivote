e4575bf26dac278ed486c1214c64f765c491aa31
Martin Sander <martin.sander@tngtech.com>
2010-11-11 20:09:51 +0000

    add progress spinner for us umts users..

--- src/main/resources/techday.js
+++ src/main/resources/techday.js
@@ -11,6 +11,7 @@
             var audience = that.parent().parent().find("td[ id ^= 'audience']");
             var interestImage = getInterestImage(interested);
             var contextPath = AJS.params.contextPath;
+            var img = that.find("img")
 
             // TODO use property for url
             if (typeof(contextPath) == undefined) {
@@ -22,9 +23,12 @@
                      {name: "interested", value: interested}]);
 
             $.ajax({type: "POST", dataType: "json", url: url, data: "",
+            beforeSend: function(data) {
+            	img.attr("src", AJS.params.progressImage)
+            },
             success: function(data) {
+            	//window.setTimeout(function () {
                 that.attr("data-interest", !interested);
-                var img = that.find("img")
                 img.attr("src", interestImage)
                 audience.text(data.userNo);
                 audience.attr("title", data.users);
@@ -32,7 +36,7 @@
                     that.parent().parent().attr("class", "interested")
                 } else {
                     that.parent().parent().attr("class", "notInterested")
-                }
+                }//}, 500);
             }});
 
             return false;
