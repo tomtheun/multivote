c658a63d2801b5b457a8df899a7395ee296f76b6
Martin Sander <martin.sander@tngtech.com>
2012-02-04 16:37:33 +0000

    whitespace

--- src/main/java/com/tngtech/confluence/plugin/data/ItemKey.java
+++ src/main/java/com/tngtech/confluence/plugin/data/ItemKey.java
@@ -6,19 +6,21 @@ public class ItemKey {
     private String itemId;
     private String tableId;
     private ContentEntityObject page;
-    
+
     public ItemKey(ContentEntityObject page, String tableId, String itemId) {
         this.page = page;
         this.tableId = tableId;
         this.itemId = itemId;
     }
-    
+
     public String getItemId() {
         return itemId;
     }
+
     public String getTableId() {
         return tableId;
     }
+
     public ContentEntityObject getPage() {
         return page;
     }
